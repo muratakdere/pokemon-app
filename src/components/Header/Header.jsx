c2ed8e4d96e1abe66cb73909cca7e78caaba130a
muratakdere <muratakdere@posta.mu.edu.tr>
2024-08-03 18:29:41 +0300

    add-header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className="header">
+     
+        <ul className="header-links">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Pokedex</a>
+          </li>
+          <li>
+            <a href="#">Video Games & App</a>
+          </li>
+          <li>
+            <a href="#">Trading Card Game</a>
+          </li>
+          <li>
+            <a href="#">Animation</a>
+          </li>
+        </ul>
+      </div>
+    
+  );
+};
+
+export default Header;
